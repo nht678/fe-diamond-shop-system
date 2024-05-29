adfe0fcb2992ed4d95ed826ed32b662f98f7078a
phuoctmse <phuoctmse184067@fpt.edu.vn>
2024-05-30 00:17:40 +0700

    Fixing warning

--- src/sections/promotions/create-promo-table.jsx
+++ src/sections/promotions/create-promo-table.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import PropTypes from 'prop-types';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
 
 function PromotionForm({ open, onClose, onSubmit }) {
     const initialFormState = {
