392747577577f020cb1dba69ee455be49cb226a7
Nguyen_Le_Khoi <khoinlse183712@fpt.edu.vn>
2024-05-30 20:28:47 +0700

    Update customer

--- src/_mock/customer.js
+++ src/_mock/customer.js
@@ -4,13 +4,15 @@ export const customer =[
     address: '32 Western Street',
     phoneNumber:'(84) 453-7283',
     point: ' 43 ',
+    gender: 'male'
   },
 
   {
     name: 'John Smith',
     address: '4324 Las Vegas ',
-    phoneNumber:'(321) 987-4311',
+    phoneNumber:'(31) 987-4311',
     point: ' 21 ',
+    gender: 'male'
   },
 
   {
@@ -18,6 +20,7 @@ export const customer =[
     address: '12 Califonia',
     phoneNumber:'(84) 453-7283',
     point: ' 43 ',
+    gender: 'male'
   },
 
   {
@@ -25,6 +28,7 @@ export const customer =[
     address: '452 Los Angeles',
     phoneNumber:'(64) 422-5632',
     point: ' 100 ',
+    gender:' female'
   },
 
   {
@@ -32,5 +36,6 @@ export const customer =[
     address: '452 Los Angeles',
     phoneNumber:'(23) 543-1892',
     point: ' 65 ',
+    gender: 'female'
   },
 ]
